0badfcb7b8fa0072c3b9accfe98e5d8ca63bb162
sandip202223 <sandip.sharma@people10.com>
2024-01-11 21:54:36 +0530

    routing request

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-  console.log(req.url,req.method)  
+  const url = req.url;
+  if(url === '/'){
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>');
+    res.write('<head>Hello,</head>');
+    res.write('<form action="/message" method="POST"><input type="text" name="message"></input></form>');
+    res.write('</html>');
+    return res.end()
+  }
   res.setHeader('Content-Type','text/html')
   res.write('<html>');
   res.write('<head>Hello,</head>');
