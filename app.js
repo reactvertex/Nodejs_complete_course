b33894ca8e49ab394009a6bac8f6a18fba321749
sandip202223 <sandip.sharma@people10.com>
2024-01-11 21:46:21 +0530

    request and responce method

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req,res) =>{
-  console.log(req)  
+  console.log(req.url,req.method)  
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>');
+  res.write('<head>Hello,</head>');
+  res.write('<body>It is first page</body>');
+  res.write('</html>');
+  res.end();
 })
 
 server.listen(3000)
